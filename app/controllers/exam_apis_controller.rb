1a481fe9872f52d558b3bdd394230fc3d0504e89
nikhil thomas <nikhilthomas88@gmail.com>
2017-04-22 12:35:49 +0530

    second api

--- app/controllers/exam_apis_controller.rb
+++ app/controllers/exam_apis_controller.rb
@@ -1,11 +1,30 @@
 class ExamApisController < ApplicationController
+	my_token= "abcdef"
 
-	def list_exams
-      @exam = Exam.find[:exams]
-	  content_type  :json
-	  return @exam.to_json
+	def exams 
+      rec_token=params[:token]
+      if my_token !=rec_token
+        response = {:result=>"error"}
+      	return response.to_json
+      else
+        @exam = Exam.select("exams")
+	    content_type  :json
+	    return @exam.to_json
+	  end
     end
+    
 
+    def student_name 
+
+      rec_token=params[:token]
+      if my_token !=rec_token
+        response = {:result=>"error"}
+      	return response.to_json
+      else
+       @student= Student.select("students")
+       return @student.to_json
+      end
+    end
 
 
 end
