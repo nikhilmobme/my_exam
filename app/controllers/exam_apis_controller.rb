d68888f69ec9fce5648cc1ca4a3d675a7922b5c9
nikhil thomas <nikhilthomas88@gmail.com>
2017-04-22 13:52:11 +0530

    rspec and all apis

--- app/controllers/exam_apis_controller.rb
+++ app/controllers/exam_apis_controller.rb
@@ -26,5 +26,39 @@ class ExamApisController < ApplicationController
       end
     end
 
+    def marks
+      rec_token=params[:token]
+      if my_token !=rec_token
+        response = {:result=>"error"}
+      else
+        @mark =Mark.new(marks_params)
+        if @mark.save
+      	  response ={:result=>"success"}
+        else
+       	  response = {:result=>"error"}
+        end
+      end
+      return response.to_json
+    end
 
-end
+    private def marks_params
+      params.require(:mark).permit(:marks)
+    end
+
+    def student_info
+    	@student=Student.find_by(:id =>params[:student-id])
+    	@student_info.name=@student.students
+    	@exam=Exam.find_by(:id=>params[:exam-id])
+    	@student_info.exam=@exam.exams
+    	@mark=Mark.find_by(:id =>params[:student-id])
+    	@subject=@mark.subjects
+    	@mark=@mark.marks
+    	@subject.each do |sub|
+
+        end 
+
+     end
+ end
+
+
+
