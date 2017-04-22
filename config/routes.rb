3b6fb3634f0d425c7676188ee6a69460b9234ac7
nikhil thomas <nikhilthomas88@gmail.com>
2017-04-22 13:17:02 +0530

    routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
+	get 'exams'=>'exam_apis#exams'
+	get 'students' =>'exam_apis#students'
+	post 'marks'  => 'exam_apis#marks'
+	get  'student_info' => 'exam_apis#student_info'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
